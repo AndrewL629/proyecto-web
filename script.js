d6d1c4a20004deaafac7f36665d7a9d7d575d207
AndrewL629 <109176399+AndrewL629@users.noreply.github.com>
2024-05-25 17:57:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let mostrador = document.getElementById("mostrador");
+let seleccion = document.getElementById("seleccion");
+let imgSeleccionada = document.getElementById("img");
+let modeloSeleccionado = document.getElementById("modelo");
+let descripSeleccionada = document.getElementById("descripcion");
+let precioSeleccionado = document.getElementById("precio");
+let sliderInner = document.querySelector(".slider--inner");
+let index = 1;
+let images = sliderInner.querySelectorAll("image");
+
+setInterval (function (){
+    let percentage = index * -100;
+    sliderInner.style.transform = "translateX("+percentage+"%)";
+    index++;
+    if (index > (images.length - 1)){
+        index = 0;
+    }
+},3000);
+
+
+function cargar(item){
+    quitarBordes();
+    mostrador.style.width = "60%";
+    seleccion.style.width = "40%";
+    seleccion.style.opacity = "1";
+    item.style.border = "2px solid red";
+
+    imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
+
+    modeloSeleccionado.innerHTML =  item.getElementsByTagName("p")[0].innerHTML;
+
+    descripSeleccionada.innerHTML = "Selecciona la cantidad que deseas adquirir de tu producto.";
+
+    precioSeleccionado.innerHTML =  item.getElementsByTagName("span")[0].innerHTML;
+
+
+}
+function cerrar(){
+    mostrador.style.width = "100%";
+    seleccion.style.width = "0%";
+    seleccion.style.opacity = "0";
+    quitarBordes();
+}
+function quitarBordes(){
+    var items = document.getElementsByClassName("item");
+    for(i=0;i <items.length; i++){
+        items[i].style.border = "none";
+    }
+}
